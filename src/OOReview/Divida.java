6a7627bdf43ef5795a972f5ecd135621e1796be9
IzaelEffemberg <izaelcosta@icloud.com>
2017-11-03 14:37:38 -0300

    add methods get and set and the method of pay the debt

--- src/OOReview/Divida.java
+++ src/OOReview/Divida.java
@@ -1,8 +1,41 @@
 package OOReview;
 
 public class Divida {
+
     private double total;
     private double valorPago;
     private String credor;
     private String cnpjDoCredor;
+
+    public double getTotal() {
+        return this.total;
+    }
+
+    public double getValorPago() {
+        return this.valorPago;
+    }
+
+    public String getCredor() {
+        return this.credor;
+    }
+
+    public String getCnpjDoCredor() {
+        return this.cnpjDoCredor;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public void setCredor(String credor) {
+        this.credor = credor;
+    }
+
+    public void setCnpjDoCredor(String cnpjDoCredor) {
+        this.cnpjDoCredor = cnpjDoCredor;
+    }
+
+    public void pagar (double valor){
+        this.valorPago += valor;
+    }
 }
