f645cab258a9ad5b09b3173771e13a902f0dfd73
IzaelEffemberg <izaelcosta@icloud.com>
2017-11-03 16:24:41 -0300

    save the dividas that was created in registrarDivida method

--- src/BalancoEmpresa.java
+++ src/BalancoEmpresa.java
@@ -1,12 +1,17 @@
 import OOReview.Divida;
 
+import java.util.HashMap;
+
 public class BalancoEmpresa {
+
+    private HashMap<String, Divida> dividas = new HashMap<String, Divida>();
 
     public void registrarDivida(String credor, String cnpjDoCredor, double valor) {
         Divida divida = new Divida();
         divida.setCredor(credor);
         divida.setTotal(valor);
         divida.setCnpjDoCredor(cnpjDoCredor);
+        dividas.put(cnpjDoCredor, divida);
     }
 
 }
