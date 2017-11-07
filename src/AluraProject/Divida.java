78252f142c9a6cf139c711f13408f56486f3e980
IzaelEffemberg <izaelcosta@icloud.com>
2017-11-07 11:11:13 -0300

    Created scope of methods of CNPJ verification

--- src/AluraProject/Divida.java
+++ src/AluraProject/Divida.java
@@ -1,11 +1,87 @@
 package AluraProject;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Divida {
 
     private double total;
     private double valorPago;
     private String credor;
     private String cnpjDoCredor;
+    private ArrayList<Pagamento> pagamentos = new ArrayList<Pagamento>();
+
+    public void pagar (double valor){
+        if (valor < 0){
+            throw new ValorInvalidoException();
+        }
+        // taxa bancaria
+        valor = (valor > 100) ? valor - 8 : valor;
+        this.valorPago += valor;
+    }
+
+    public ArrayList<Pagamento> pagamentosAntesDe(Calendar data) {
+        ArrayList<Pagamento> pagamentosFiltrados = new ArrayList<Pagamento>();
+        for (Pagamento pagamento : this.pagamentos) {
+            if (pagamento.getData().before(data)) {
+                pagamentosFiltrados.add(pagamento);
+            }
+        }
+        return pagamentosFiltrados;
+    }
+
+    public ArrayList<Pagamento> pagamentosComValorMaiorQue(double valorMinimo) {
+        ArrayList<Pagamento> pagamentosFiltrados = new ArrayList<Pagamento>();
+        for (Pagamento pagamento : this.pagamentos) {
+            if (pagamento.getValor() > valorMinimo) {
+                pagamentosFiltrados.add(pagamento);
+            }
+        }
+        return pagamentosFiltrados;
+    }
+    public ArrayList<Pagamento> pagamentosDo(String cnpjPagador) {
+        ArrayList<Pagamento> pagamentosFiltrados = new ArrayList<Pagamento>();
+        for (Pagamento pagamento : this.pagamentos) {
+            if (pagamento.getCnpjPagador().equals(cnpjPagador)) {
+                pagamentosFiltrados.add(pagamento);
+            }
+        }
+        return pagamentosFiltrados;
+    }
+    private int primeiroDigitoCorretoParaCnpj() {
+        // Calcula o primeiro dígito verificador correto para
+        // o CNPJ armazenado no atributo valor
+        return 0;
+    }
+    private int primeiroDigitoVerificadorDoCnpj() {
+        // Extrai o primeiro dígito verificador do CNPJ armazenado
+        // no atributo valor
+        return 0;
+    }
+    public void registra(Pagamento pagamento) {
+        this.pagamentos.add(pagamento);
+        pagar(pagamento.getValor());
+    }
+    private int segundoDigitoCorretoParaCnpj() {
+        // Calcula o segundo dígito verificador correto para
+        // o CNPJ armazenado no atributo valor
+        return 0;
+    }
+    private int segundoDigitoVerificadorDoCnpj() {
+        // Extrai o segundo dígito verificador do CNPJ armazenado
+        // no atributo valor
+        return 0;
+    }
+
+    public void setCredor(String credor) {
+        this.credor = credor;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    public double valorAPagar() {
+        return this.total - this.valorPago;
+    }
 
     public double getTotal() {
         return this.total;
@@ -23,24 +99,7 @@ public class Divida {
         return this.cnpjDoCredor;
     }
 
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    public void setCredor(String credor) {
-        this.credor = credor;
-    }
-
     public void setCnpjDoCredor(String cnpjDoCredor) {
         this.cnpjDoCredor = cnpjDoCredor;
-    }
-
-    public void pagar (double valor){
-        if (valor < 0){
-            throw new ValorInvalidoException();
-        }
-        // taxa bancaria
-        valor = (valor > 100) ? valor - 8 : valor;
-        this.valorPago += valor;
     }
 }
